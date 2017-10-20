4eaf96d6142ad755adc60ac289d71ed4e5747df2
Andrew Minion <andrew@andrewrminion.com>
2017-10-19 20:32:51 -0400

    update aimtell code

--- unifiedps-aimtell.js
+++ unifiedps-aimtell.js
@@ -1,8 +1,5 @@
-var _at = {};
-_at.domain = 'unifiedps.com';
-_at.owner = '72ee96a13b6f';
-_at.idSite = '4933';
-_at.attributes = {};
-_at.webpushid = 'web.2.aimtell.com';
-(function() { var u='//s3.amazonaws.com/cdn.aimtell.com/trackpush/';var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'trackpush.min.js'; s.parentNode.insertBefore(g,s); })();
-function _aimtellPermissionGranted() {_aimtellTrackEvent("User", "Subscribed");};
+var _at = {};_at.domain = 'unifiedps.com';_at.owner = '72ee96a13b6f';_at.idSite = '4933';_at.attributes = {};_at.webpushid = 'web.2.aimtell.com';_at.worker = '/push-worker.js';(function() {
+var u='//s3.amazonaws.com/cdn.aimtell.com/trackpush/';
+var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'trackpush.min.js'; s.parentNode.insertBefore(g,s);
+})();
